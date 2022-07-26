e1c87e1e21c7d42275cbcb1ade827b93bdeff3f5
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-26 18:13:00 +0200

    Bonus funzionante

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-console.log('yo');
-
-
+// console.log('yo');
 
 const slides = [
     './img/01.jpg',
@@ -13,8 +11,7 @@ const slides = [
 // mi creo un array dove avrò le immagini già create in HTML
 const slidesElement = [];
 
-// gestisco lo stato del mio carosello con una var
-
+// gestisco lo stato del mio carosello con una variabile
 let currentIndex = 0;
 // recuperare il contenitore dello slider
 const slidesWrapperEl = document.querySelector('.slider-wrapper');
@@ -22,11 +19,11 @@ const slidesWrapperEl = document.querySelector('.slider-wrapper');
 slidesWrapperEl.innerHTML = '';
 console.log(slidesWrapperEl);
 
-// generare il contenuto dello slider
 
+// Genero il contenuto dello slider
 for (let i = 0; i < slides.length; i++) {
     const src = slides[i];
-    console.log(src);
+    // console.log(src);
     // Creo il'li
     const li = document.createElement('li');
     // Aggiungo la classe slide a questo li
@@ -39,15 +36,16 @@ for (let i = 0; i < slides.length; i++) {
     const img = document.createElement('img');
     // assegno all'immagine creata il valore letto dall'array
     img.src = src;
-    console.dir(img);
+    // console.dir(img);
     // a questo punto appendo l'immagine dentro l'li creato
     li.append(img);
     // Appendo il mio li creato con l'img dentro allo slide wrapper precendentemente selezionato
     slidesWrapperEl.append(li);
     // le inserisco nel mio array vuoto precedentemente
     slidesElement.push(li);
-};
+}
     
+
 
 
 
@@ -55,51 +53,53 @@ for (let i = 0; i < slides.length; i++) {
 const nextArrowEl = document.querySelector('.arrow_next');
 // Devo ascoltare l'evento click sulla arrow next
 nextArrowEl.addEventListener('click', function () {
-    console.log('tasto next premuto');
+    // console.log('tasto next premuto');
     // togliere la classe active della slide attiva
     const slideAttiva = slidesElement[currentIndex];
-    console.log(slideAttiva);
+    // console.log(slideAttiva);
     slideAttiva.classList.remove('active');
+    // mi vado a inizializzare la slideSuccessiva e a fare un controllo dell'index per ripetere il ciclo delle immagini
+    let slideSuccessiva
+    if (currentIndex === 4) {
+        currentIndex = 0
+    } else {
+        currentIndex++
+    }
     // aggiungere la classe active alla slide successiva (se esiste)
-    const slideSuccessiva = slidesElement[currentIndex + 1];
-    console.log(slideSuccessiva);
+    slideSuccessiva = slidesElement[currentIndex];
+    // console.log(slideSuccessiva);
     slideSuccessiva.classList.add('active');
-    // Aumento l'idice di 1 così la prossima slide attiva sarà la slide di indice 1 e così via
-    currentIndex++;
-    console.log(currentIndex);
-    // if (currentIndex === 4) {
-    //     currentIndex = 0;
-    //     console.log('currentIndex modificato dall if')
-    // };
 }); 
+  
+    
 
 // Seleziono la mia freccia prev
 const prevArrowEl = document.querySelector('.arrow_prev');
 // Devo ascoltare l'evento click sulla arrow previous
 prevArrowEl.addEventListener('click', function() {
-    console.log('tasto prev premuto');
+    // console.log('tasto prev premuto');
     // tolgo la classe active dalla slide attiva che in questo caso sarà
     const slideAttiva = slidesElement[currentIndex];
-    console.log(slideAttiva);
+    // console.log(slideAttiva);
     slideAttiva.classList.remove('active');
+    // mi vado a inizializzare la slidePrecedente e a fare un controllo sull'index per ripete il ciclo delle img
+    let slidePrecedente
+    if (currentIndex === 0) {
+        currentIndex = 4
+    } else {
+        currentIndex--
+    }
     // Aggiungo la classe active alla slide precedente
-    const slidePrecedente = slidesElement [currentIndex - 1];
-    console.log(slidePrecedente)
+    slidePrecedente = slidesElement [currentIndex];
+    // console.log(slidePrecedente)
     slidePrecedente.classList.add('active')
-    // Diminuisco l'indice perchè sta volta sto tornando indietro
-    currentIndex--;
-    console.log(currentIndex);
-    // if (currentIndex === -1) {
-    //     currentIndex = 5
-    // }
-});
+})
+    
+   
 
 
-//  if (currentIndex === 6) {
-//      currentIndex = 0
-// } else if (currentIndex === -1) {
-//      currentIndex = 5
-// }
+
+
 
 
 
